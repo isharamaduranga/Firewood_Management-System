3a96a3f58d9293e5050174a036333c0ff5fc76c0
Ishara Maduranga <99071775+isharamaduranga@users.noreply.github.com>
2023-06-16 00:00:36 +0530

    Implemented basic file structure with Back-End & added basic Configurations...

---
+++ Back-End/src/main/java/config/JPAConfig.java
@@ -0,0 +1,64 @@
+/**
+ * @author : Ishara Maduarnga
+ * Project Name: firewood_management
+ * Date        : 6/15/2023
+ * Time        : 11:55 PM
+ * Year        : 2023
+ */
+
+package config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.JpaVendorAdapter;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.Database;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import javax.persistence.EntityManagerFactory;
+import javax.sql.DataSource;
+
+@EnableJpaRepositories(basePackages = "repo")
+@EnableTransactionManagement
+public class JPAConfig {
+
+    @Bean
+    public LocalContainerEntityManagerFactoryBean entityManagerFactory(DataSource ds, JpaVendorAdapter jpa){
+        LocalContainerEntityManagerFactoryBean bean = new LocalContainerEntityManagerFactoryBean();
+        bean.setPackagesToScan("entity");
+        bean.setDataSource(ds);
+        bean.setJpaVendorAdapter(jpa);
+        return bean;
+    }
+
+    @Bean
+    public  DataSource dataSource(){
+        //We use this data source Only For accessing MYSQL database
+        DriverManagerDataSource ds = new DriverManagerDataSource();
+        ds.setDriverClassName("com.mysql.cj.jdbc.Driver");
+        ds.setUrl("jdbc:mysql://localhost:3306/firewood?createDatabaseIfNotExist=true");
+        ds.setUsername("root");
+        ds.setPassword("1234");
+        return ds;
+    }
+    /** org.hibernate.dialect.MySQL8Dialect */
+    @Bean
+    public JpaVendorAdapter jpaVendorAdapter(){
+        HibernateJpaVendorAdapter va = new HibernateJpaVendorAdapter();
+        va.setDatabasePlatform("org.hibernate.dialect.MySQL8Dialect");
+        va.setDatabase(Database.MYSQL);
+        va.setGenerateDdl(true);
+        va.setShowSql(true);
+        return va;
+    }
+
+    @Bean
+    public PlatformTransactionManager transactionManager(EntityManagerFactory emf){
+        return new JpaTransactionManager(emf);
+    }
+
+}
