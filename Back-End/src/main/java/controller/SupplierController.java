6d333840e50af0235b972146a98f95f25916a67d
Ishara Maduranga <99071775+isharamaduranga@users.noreply.github.com>
2023-06-16 11:15:01 +0530

    Created controller class SupplierController...

---
+++ Back-End/src/main/java/controller/SupplierController.java
@@ -0,0 +1,17 @@
+/**
+ * @author : Ishara Maduarnga
+ * Project Name: firewood_management
+ * Date        : 6/16/2023
+ * Time        : 11:12 AM
+ * Year        : 2023
+ */
+
+package controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/supplier")
+public class SupplierController {
+}
