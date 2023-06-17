097a644021f5d97d480023ac26149a5e94ecbbc7
Ishara Maduranga <99071775+isharamaduranga@users.noreply.github.com>
2023-06-17 15:10:19 +0530

    Created class AdminController and AdminDTO with Backend...

---
+++ Back-End/src/main/java/controller/AdminController.java
@@ -0,0 +1,12 @@
+/**
+ * @author : Ishara Maduarnga
+ * Project Name: firewood_management
+ * Date        : 6/17/2023
+ * Time        : 3:08 PM
+ * Year        : 2023
+ */
+
+package controller;
+
+public class AdminController {
+}
---
+++ Back-End/src/main/java/dto/AdminDTO.java
@@ -0,0 +1,25 @@
+/**
+ * @author : Ishara Maduarnga
+ * Project Name: Easy_Car_Rental
+ * Date        : 2/9/2023
+ * Time        : 12:31 PM
+ * Year        : 2023
+ */
+
+package dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@ToString
+public class AdminDTO {
+    private String id;
+    private String name;
+    private String password;
+    private String contact;
+}
