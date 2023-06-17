cb11ab5b59a63c5fc3677a8fb38eced6a765e800
Ishara Maduranga <99071775+isharamaduranga@users.noreply.github.com>
2023-06-17 15:31:55 +0530

    Created admin repos and services....

---
+++ Back-End/src/main/java/repo/AdminRepo.java
@@ -0,0 +1,8 @@
+package repo;
+
+import entity.Admin;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AdminRepo extends JpaRepository<Admin,String> {
+    Admin findByPasswordAndName(String password,String name);
+}
